51fff728827411b9acae52e140306fd72de2d9cd
anupam <anupam4944@gmail.com>
2023-05-31 23:29:18 +0530

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by SKUBI on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
